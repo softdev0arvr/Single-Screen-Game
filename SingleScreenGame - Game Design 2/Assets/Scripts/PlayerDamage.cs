e2771fccd20a41db775ef1152c666fbedc461812
softdev0arvr <gradyfrank6@gmail.com>
2021-10-20 19:17:21 +0100

    imporved ai pathing

--- SingleScreenGame - Game Design 2/Assets/Scripts/PlayerDamage.cs
+++ SingleScreenGame - Game Design 2/Assets/Scripts/PlayerDamage.cs
@@ -9,7 +9,7 @@ public class PlayerDamage : MonoBehaviour
     [SerializeField] private Collider playerCheck;
     [SerializeField] private LayerMask playerLayers;
     [SerializeField] private GameManager manager;
-    [SerializeField] private int scoreToGive;
+    //[SerializeField] private int scoreToGive;
 
 
     private void OnCollisionEnter(Collision other)
@@ -17,10 +17,10 @@ public class PlayerDamage : MonoBehaviour
         if (other.gameObject.tag == "Enemy")
         {
             
-            transform.position = new Vector3(13.1f,1.3f,-42.71464f);
+            transform.position = new Vector3(22.89161f,4.205579f,-42.29498f);
 
-            transform.position = new Vector3(0f,0f,0f);
-            manager.AddScore(scoreToGive);
+            
+            //manager.AddScore(scoreToGive);
 
         }
     }
